dc7968553cc02a434b80fa30e1cc0f9da0e18e90
viswa <18b01a02a2@svecw.edu.in>
2022-12-11 18:59:00 +0530

    user products

---
+++ src/Components/UserProCard/UserProCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './UserProCard.css';
+
+const UserProCard = (props) => {
+    //const { name, price, img } = item;
+    return (
+        <div className="aml_cards">
+            <div className="aml_image_box">
+                <img src={props.img} alt="Image" />
+            </div>
+            <div className="aml_details">
+                <p>{props.name}</p>
+                <p>Price - {props.price}Rs</p>
+                <button onClick={props.addToCart}>Add to Cart</button>
+            </div>
+        </div>
+    );
+};
+
+export default UserProCard
